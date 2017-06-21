b5a2379746be37adeda14048db7e95d1f8003d6f
RQM- <benoit.jacques.felix@gmail.com>
2017-06-21 11:48:54 +0200

    Téléchargements d'en un fichier XML automatique sans création de fichier

--- exportToXML.php
+++ exportToXML.php
@@ -1,32 +1,20 @@
-<!DOCTYPE html>
-<html>
-  <head>
-  <title>Export to XML</title>
-  <meta charset="UTF-8">
-  <link href="main.css" rel="stylesheet">
-  </head>
-  <body>
-      <pre>
-      <?php
-        $Programme = $_POST['Programme'];
-        $LanguageModule = $_POST['LanguageModule'];
-        $GroupeModule = $_POST['GroupeModule'];
-        $ObjectifModule = $_POST['ObjectifModule'];
-        $Prerequis = $_POST['Prerequis'];
-        $Materiel = $_POST['Materiel'];
-        $Sequence = $_POST['Sequence'];
-        $ObjectifSequence = $_POST['ObjectifSequence'];
-        $CodeSequence = $_POST['CodeSequence'];
-        $done = $_POST['done'];
-        $Objectif = $_POST['Objectif'];
-        $Code = $_POST['Code'];
-        $Temps = $_POST['Temps'];
-        $SousObjectif = $_POST['SousObjectif'];
-        $Description = $_POST['Description'];
-        
-
-        $string = "
-<?xml version='1.0' encoding='UTF-8'?>
+<?php
+  $Programme = $_POST['Programme'];
+  $LanguageModule = $_POST['LanguageModule'];
+  $GroupeModule = $_POST['GroupeModule'];
+  $ObjectifModule = $_POST['ObjectifModule'];
+  $Prerequis = $_POST['Prerequis'];
+  $Materiel = $_POST['Materiel'];
+  $Sequence = $_POST['Sequence'];
+  $ObjectifSequence = $_POST['ObjectifSequence'];
+  $CodeSequence = $_POST['CodeSequence'];
+  $done = $_POST['done'];
+  $Objectif = $_POST['Objectif'];
+  $Code = $_POST['Code'];
+  $Temps = $_POST['Temps'];
+  $SousObjectif = $_POST['SousObjectif'];
+  $Description = $_POST['Description'];
+  $string = "<?xml version='1.0' encoding='UTF-8'?>
 <Programme formation='$Programme'>
   <Module language='$LanguageModule' group='$GroupeModule'>
     <Objectif>$ObjectifModule</Objectif>
@@ -45,10 +33,8 @@
     </Sequence>
   </Module>
 </Programme>";
-
-        echo htmlentities($string);
-        ?>
-      </pre>
-  </body>
-</html>
-
+        header('Content-Description: File Transfer');
+        header('Content-Type: text/xml');
+        header('Content-Disposition: attachment; filename="downloaded.xml"');
+        print_r($string);
+?>
